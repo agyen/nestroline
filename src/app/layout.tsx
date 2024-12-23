a4120156dac53d4ddb6d26b57d4af8be7cd39be9
Isaac Agyen <mac@MACs-MacBook-Pro.local>
2024-12-23 12:57:55 +0000

    Update script for analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Toaster } from 'react-hot-toast';
 import AosProvider from '@/providers/AosProvider';
+import Script from 'next/script'
+
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,6 +25,11 @@ export default function RootLayout({
           {children}
           <Toaster position="top-right" />
         </AosProvider>
+        <Script
+          defer 
+          data-website-id="d4bc2c16-e693-4abe-bb15-322006b9297b" 
+          data-domain="https://nestroline.com" 
+          src="https://analytics-5qai.onrender.com/static/tracker.js" />
       </body>
     </html>
   );
