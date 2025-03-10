a0e11bac5daf7ba695ef4e29a35f95bddbfe69e6
Isaac Agyen <mac@MACs-MacBook-Pro.local>
2025-03-10 22:30:19 +0000

    Added posthog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,22 @@ import { Hero } from '@/components/sections/Hero';
 import { Services } from '@/components/sections/Services';
 import { About } from '@/components/sections/About';
 import { Contact } from '@/components/sections/Contact';
+import { PostHogProvider } from 'posthog-js/react';
+
+const options = {
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || '',
+}
 
 export default function Home() {
   return (
     <main className="bg-slate-50">
+      <PostHogProvider apiKey={process.env.NEXT_PUBLIC_POSTHOG_KEY || ''} options={options}>
       <Navbar />
       <Hero />
       <Services />
       <About />
       <Contact />
+      </PostHogProvider>
     </main>
   );
 }
